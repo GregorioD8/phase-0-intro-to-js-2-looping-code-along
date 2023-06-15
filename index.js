06e110e45d2f7192416c251b55622a9865aedbbf
Gregorio <Greg.del801@gmail.com>
2023-06-15 16:47:44 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+
+function writeCards(names, event) {
+    const cards = [];
+
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  console.log(cards);
+  return cards;
+}
+
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+function countDown(number) {
+  let countdown = number;
+      while (countdown >= 0) {
+        console.log(countdown--);
+        }
+}
+
+countDown(10);
